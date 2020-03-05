56173269cff7edbd771f91c6dba95c843492131f
David Quenault <david.quenault@morrisonsplc.co.uk>
2020-03-05 09:42:53 +0000

    secured credentials

--- install.sh
+++ install.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
-# Initial commands to install ansible / git to be able to run the provisioning scripts
-
+# Initial commands to install ansible & homebrew to be able to run the provisioning scripts
 # Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" > /dev/null 2>&1
 
 curl https://bootstrap.pypa.io/get-pip.py | python
 pip install ansible
-
-#brew install pyenv
-#pyenv install 3.8.1
-#python -m venv venv
-#source venv/bin/activate
-#pip install ansible
 
 # Install the pre-reqs for ansible imported roles
 ansible-galaxy install -r requirements.yml
