439b71e5fdd71e7de9b2db3312fbb5eedc62a7a7
David Quenault <david.quenault@morrisonsplc.co.uk>
2020-03-05 13:28:31 +0000

    Leveraged homebrew role by geerlingguy to simplify script

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Initial commands to install ansible & homebrew to be able to run the provisioning scripts
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" > /dev/null 2>&1
 
 curl https://bootstrap.pypa.io/get-pip.py | python
 pip install ansible
