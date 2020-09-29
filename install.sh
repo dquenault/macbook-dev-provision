2dbc217f1343908b702ad5f4b52ceb28ce9ad14f
David Quenault <davidquenault@anddigitaldqmbp.lan>
2020-09-29 09:26:07 +0100

    Updated to leverage user install vs global admin install of pip

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Initial commands to install ansible & homebrew to be able to run the provisioning scripts
 
-curl https://bootstrap.pypa.io/get-pip.py | python
+# Install xcode tools first
+#xcode-select --install
+
+
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python /tmp/get-pip.py --user
+# Add pip to path
+export PATH=$PATH:~/Library/Python/2.7/bin
+
 pip install ansible
 
 # Install the pre-reqs for ansible imported roles
